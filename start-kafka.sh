eb678ecd5d944248b59f972ea58999ea89154162
vandanafs <vandana.f.09@gmail.com>
2022-06-09 13:52:42 -0400

    added

--- start-kafka.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-MACHARCH=`uname -m`
-MACM1="arm64"
-
-if [[ "$MACHARCH" == "$MACM1" ]]; then
-   /opt/homebrew/opt/kafka/bin/kafka-server-start /opt/homebrew/etc/kafka/server.properties
-else
-   kafka-server-start /usr/local/etc/kafka/server.properties
-fi
